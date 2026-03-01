aeff4db686ce3b0459cfea85af0f6388bb0c0038
Sanjiv39 <sanjiv.samal39@gmail.com>
2026-03-02 00:07:23 +0530

    changes : dev env updated + api configs added

--- src/services/apis/config.ts
+++ src/services/apis/config.ts
@@ -21,55 +21,58 @@ const jsonContentHeadersAuth = {
   Authorization: `Bearer ${token}`,
 };
 
-export const AUTH = axios.create({
-  baseURL: baseUrl + "/auth",
-  headers: jsonContentHeaders,
-  withCredentials: true,
-});
+// Admin configs
 
 export const ADMIN = axios.create({
   baseURL: baseUrl + "/admin",
   headers: jsonContentHeadersAuth,
   withCredentials: true,
 });
-export const ADMIN_USERS = ADMIN.create({
-  baseURL: baseUrl + "/admin/users",
+export const ADMIN_AUTH = axios.create({
+  baseURL: baseUrl + "/admin/auth",
   headers: jsonContentHeadersAuth,
   withCredentials: true,
 });
-export const ADMIN_CHANNEL = ADMIN.create({
-  baseURL: baseUrl + "/admin/channel",
+export const ADMIN_ENTERPRISE = ADMIN.create({
+  baseURL: baseUrl + "/admin/enterprises",
   headers: jsonContentHeadersAuth,
   withCredentials: true,
 });
-export const ADMIN_PLAYLIST = ADMIN.create({
-  baseURL: baseUrl + "/admin/playlist",
+export const ADMIN_ADMIN = ADMIN.create({
+  baseURL: baseUrl + "/admin/admins",
+  headers: jsonContentHeadersAuth,
+  withCredentials: true,
+});
+export const ADMIN_BRANCH = ADMIN.create({
+  baseURL: baseUrl + "/admin/branches",
+  headers: jsonContentHeadersAuth,
+  withCredentials: true,
+});
+export const ADMIN_SPACE = ADMIN.create({
+  baseURL: baseUrl + "/admin/spaces",
   headers: jsonContentHeadersAuth,
   withCredentials: true,
 });
 
-export const ACCOUNT = axios.create({
-  baseURL: baseUrl + "/account",
+// Enterprise
+
+export const ENTERPRISE = axios.create({
+  baseURL: baseUrl + "/enterprise",
   headers: jsonContentHeadersAuth,
   withCredentials: true,
 });
-export const USERS = axios.create({
-  baseURL: baseUrl + "/users",
+export const ENTERPRISE_AUTH = axios.create({
+  baseURL: baseUrl + "/enterprise/auth",
   headers: jsonContentHeadersAuth,
   withCredentials: true,
 });
-export const GROUP = axios.create({
-  baseURL: baseUrl + "/group",
+export const ENTERPRISE_BRANCH = ENTERPRISE.create({
+  baseURL: baseUrl + "/enterprise/branches",
   headers: jsonContentHeadersAuth,
   withCredentials: true,
 });
-export const CHANNEL = axios.create({
-  baseURL: baseUrl + "/channel",
-  headers: jsonContentHeadersAuth,
-  withCredentials: true,
-});
-export const COMMON = axios.create({
-  baseURL: baseUrl + "/common",
+export const ENTERPRISE_SPACE = ENTERPRISE.create({
+  baseURL: baseUrl + "/enterprise/spaces",
   headers: jsonContentHeadersAuth,
   withCredentials: true,
 });
