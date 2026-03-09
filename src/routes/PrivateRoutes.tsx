c7d29d90c165014722d69dacf2bf02d41ed1a9fc
Suvam1998 <2006297@kiit.ac.in>
2026-03-10 01:07:13 +0530

    Privateroutes operator(operatorspace)

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -4,62 +4,41 @@ import {
   useCallback,
   useEffect,
   useMemo,
-  type ComponentType,
-  type JSX,
-  type LazyExoticComponent,
   type ReactNode,
 } from "react";
+
 import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import TopBarProgress from "react-topbar-progress-indicator";
 import { toast } from "sonner";
+
 import { useUser } from "@/services/hooks/use-user";
-// import { useTawk } from "@/services/hooks/use-tawk";
 import { validateNumber } from "@/utils/number";
-// import { datifyObjectValues } from "@/utils/object/datify";
 import { getCssVariableValue } from "@/utils/css-variable";
+
 import Layout from "@/components/layout/Layout";
 import RotatingLoader from "@/components/loaders/rotating";
 import ActionButton from "@/components/buttons/action-btn";
-import SpaceOperatorPage from "@/pages/space-operator";
-import SpaceOperator from "@/pages/space-operator";
-import SpaceEditPage from "@/pages/display/space-edit-page";
 
-// Lazy load components
 const Dashboard = lazy(() => import("@/pages/dashboard"));
-// const Users = lazy(() => import("@/pages/users"));
 const Display = lazy(() => import("@/pages/display"));
-// const Settings = lazy(() => import("@/pages/settings"));
-
+const OperatorsPage = lazy(() => import("@/pages/operators"))
+const OperatorSpacesPage = lazy(() => import("@/pages/operators/operator-spaces"))
 interface SuspensedViewProps {
   children: ReactNode;
 }
 
 const PrivateRoutes = () => {
-  // useTawk();
-
   const navigate = useNavigate();
 
   const {
     data: res,
     isFetching,
     tokenStoreState,
-    userLevel,
-    status,
-    fetchCount,
   } = useUser();
 
   const AutoNavigateRender = useCallback(
-    ({
-      El,
-    }: Partial<{
-      El: ReactNode;
-    }>) =>
-      // fetchCount > 0 && status === "success" ? (
-      //   <Navigate to={"/settings"} />
-      // ) : (
-      El,
-    // )
-    [fetchCount, status, userLevel],
+    ({ El }: { El?: ReactNode }) => El,
+    []
   );
 
   const isExpired = useMemo(
@@ -68,104 +47,99 @@ const PrivateRoutes = () => {
       validateNumber(tokenStoreState.value?.expiry?.getTime(), {
         invalidValue: 0,
       }),
-    [tokenStoreState.value],
+    [tokenStoreState.value]
   );
 
   useEffect(() => {
     if (!res?.data?.data?.id && !isFetching) {
-      toast.error("Something wrong ! Please relogin");
+      toast.error("Something wrong! Please relogin");
     }
   }, [isFetching, res?.data?.data?.id]);
 
-  return !!isFetching ? (
-    <div className="w-full h-full min-h-dvh flex flex-col gap-3 justify-center items-center px-2 py-4">
-      <RotatingLoader className="size-[50px] text-accent-foreground"></RotatingLoader>
-      <p className="text-xl font-medium text-muted-foreground">
-        Please Wait.....
-      </p>
-    </div>
-  ) : !res?.data?.data?.id ? (
-    <div className="text-accent-foreground text-lg font-medium flex flex-col gap-4 items-center justify-center h-full">
-      {!tokenStoreState.value?.token
-        ? "You need to login with your account before proceeding"
-        : isExpired
+  if (isFetching) {
+    return (
+      <div className="w-full h-full min-h-dvh flex flex-col gap-3 justify-center items-center px-2 py-4">
+        <RotatingLoader className="size-[50px] text-accent-foreground" />
+        <p className="text-xl font-medium text-muted-foreground">
+          Please Wait.....
+        </p>
+      </div>
+    );
+  }
+
+  if (!res?.data?.data?.id) {
+    return (
+      <div className="text-accent-foreground text-lg font-medium flex flex-col gap-4 items-center justify-center h-full">
+        {!tokenStoreState.value?.token
+          ? "You need to login with your account before proceeding"
+          : isExpired
           ? "Your session has expired. Login again"
           : "We cannot get your data at the moment. Try login again"}
-      <ActionButton
-        className=""
-        onClick={() => {
-          navigate("/login");
-        }}
-      >
-        Go to Login
-      </ActionButton>
-    </div>
-  ) : (
-    <>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route
-            path="/dashboard"
-            element={
-              <SuspensedView>
-                {/* {fetchCount > 0 && status === "success" && userLevel < 1 ? (
-                  <Navigate to={"/settings"} />
-                ) : (
-                  <Dashboard />
-                )} */}
-                <AutoNavigateRender El={<Dashboard />} />
-              </SuspensedView>
-            }
-          />
-          <Route
-            path="/display"
-            element={
-              <SuspensedView>
-                <AutoNavigateRender El={<Display />} />
-              </SuspensedView>
-            }
-          />
 
-          <Route path="/users/">
-            <Route
-              index
-              element={
-                <SuspensedView>
-                  <AutoNavigateRender El={<Dashboard />} />
-                </SuspensedView>
-              }
-            />
-          </Route>
-            <Route path="/space-edit-page/:id" element={<SpaceEditPage />} /> 
-            <Route
-              path="/space-operator"
-              element={
-                <SuspensedView>
-                  <AutoNavigateRender El={<SpaceOperator />} />
-                </SuspensedView>
-              }
-            />
+        <ActionButton
+          onClick={() => {
+            navigate("/login");
+          }}
+        >
+          Go to Login
+        </ActionButton>
+      </div>
+    );
+  }
 
-          <Route
-            path="/settings/*"
-            element={
-              <SuspensedView>
-                <AutoNavigateRender El={<Dashboard />} />
-              </SuspensedView>
-            }
-          />
+  return (
+    <Routes>
+      <Route element={<Layout />}>
 
-          <Route path="/*" element={<Navigate to="/dashboard" />} />
-        </Route>
-      </Routes>
-    </>
+        {/* Dashboard */}
+        <Route
+          path="/dashboard"
+          element={
+            <SuspensedView>
+              <AutoNavigateRender El={<Dashboard />} />
+            </SuspensedView>
+          }
+        />
+
+        {/* Display */}
+        <Route
+          path="/display"
+          element={
+            <SuspensedView>
+              <AutoNavigateRender El={<Display />} />
+            </SuspensedView>
+          }
+        />
+
+        {/* Operators Table */}
+        <Route
+          path="/operators"
+          element={
+            <SuspensedView>
+              <AutoNavigateRender El={<OperatorsPage />} />
+            </SuspensedView>
+          }
+        />
+
+        {/* Spaces under Operator */}
+        <Route
+          path="/operators/:operatorId"
+          element={
+            <SuspensedView>
+              <AutoNavigateRender El={<OperatorSpacesPage />} />
+            </SuspensedView>
+          }
+        />
+
+        {/* Default Redirect */}
+        <Route path="/*" element={<Navigate to="/dashboard" />} />
+
+      </Route>
+    </Routes>
   );
 };
 
 const SuspensedView = ({ children }: SuspensedViewProps) => {
-  const baseColor = "var(--ring)";
-
-  // console.log(getCssVariableValue("--primary"));
 
   TopBarProgress.config({
     barColors: {
@@ -179,7 +153,11 @@ const SuspensedView = ({ children }: SuspensedViewProps) => {
     shadowBlur: 5,
   });
 
-  return <Suspense fallback={<TopBarProgress />}>{children}</Suspense>;
+  return (
+    <Suspense fallback={<TopBarProgress />}>
+      {children}
+    </Suspense>
+  );
 };
 
 export default PrivateRoutes;
