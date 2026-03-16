4d7bcead928991ecf7d7524444d25ea65e0d150c
pride-spaces-sanjiv <sanjiv.samal@pridespaces.com>
2026-03-17 02:41:55 +0530

    changes : minor

--- src/database/schemas/operator.ts
+++ src/database/schemas/operator.ts
@@ -28,8 +28,22 @@ export const operatorSchema = z.object({
   email: getEmailSchema(),
   password: getPasswordSchema(),
   slug: getSlugSchema({ keyName: "Operator Slug" }),
-  gstNo: z.string().trim().min(1, "GST number is required"),
-  cinNo: z.string().trim().min(1, "CIN number is required"),
+  gstNo: z
+    .string()
+    .trim()
+    .min(1, "GST number is required")
+    .refine(
+      (v) => v.match(/^[A-Za-z0-9]{15}$/),
+      "GST number must be 15 characters long",
+    ),
+  cinNo: z
+    .string()
+    .trim()
+    .min(1, "CIN number is required")
+    .refine(
+      (v) => v.match(/^[A-Za-z0-9]{21}$/),
+      "CIN number must be 21 characters long",
+    ),
   headquarter: headQuarterSchema,
   person: headQuarterPersonSchema,
   isActive: z.boolean().optional().default(true),
