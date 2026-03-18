ee15f392f21b92ce44530847b14a0429770f785e
abhay-kumawat-301 <abhaykumawat301@gmail.com>
2026-03-19 04:02:09 +0530

    Amenities tab created

---
+++ src/pages/amenities/index.tsx
@@ -0,0 +1,12 @@
+const Amenities = () => {
+    return (
+        <div className="container mx-auto p-6">
+            <div className="flex justify-between items-center my-4">
+                <h1 className="text-2xl font-bold">This is Amenities Page!</h1>
+                {/* <UserCreateModal /> */}
+            </div>
+            {/* <UsersTabledResults /> */}
+        </div>
+    )
+};
+export default Amenities;
