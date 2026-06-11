285e64bb7662538a8f4debbe994efb7699c6da7d
Sanjiv39 <sanjiv.samal39@gmail.com>
2026-06-11 16:41:14 +0530

    fix : infer non raw object type exclusions in object

--- src/types/data/infer.ts
+++ src/types/data/infer.ts
@@ -1,5 +1,10 @@
-export type DeepInfer<T extends any> =
-  T extends Record<string, any>
+export type DeepInfer<T extends any> = T extends
+  | Date
+  | RegExp
+  | Function
+  | ((...args: any[]) => any)
+  ? T
+  : T extends Exclude<Record<string, any>, any[]>
     ? {
         [K in keyof T]: DeepInfer<T[K]>;
       }
