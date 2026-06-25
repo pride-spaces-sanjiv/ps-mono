d8799f4b9247bca6e18bb7823e22db2b540b8a87
pride-spaces-sanjiv <sanjiv.samal@pridespaces.com>
2026-06-25 23:13:58 +0530

    minor changes

--- src/utils/data/spaceTypes.ts
+++ src/utils/data/spaceTypes.ts
@@ -1,4 +1,16 @@
 export const spaceTypes = ["Flex", "MOS", "Both"] as const;
+export const labelledSpaceTypes = spaceTypes.map((v, i) => ({
+  label:
+    (v === "Both" ? "Hybrid" : v) +
+    " ( " +
+    (v === "Flex"
+      ? "Co-Working"
+      : v === "MOS"
+        ? "Managed"
+        : "Co-Working and Managed") +
+    " )",
+  value: v,
+}));
 export const spaceGrades = ["A+", "A", "B"] as const;
 
 export type SpaceType = (typeof spaceTypes)[number];
