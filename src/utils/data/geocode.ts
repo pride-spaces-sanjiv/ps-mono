03e8a813aed1f93c424e5ca884b9ad395b0e5c19
Sanjiv39 <sanjiv.samal39@gmail.com>
2026-07-02 19:46:12 +0530

    changes : util to get lat lng from maps url

--- src/utils/data/geocode.ts
+++ src/utils/data/geocode.ts
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { locationSchema } from "../schemas/location";
+
 export const parseAddress = (
   components: google.maps.GeocoderResult["address_components"],
 ) => {
@@ -35,4 +38,22 @@ export const geocodeLatLng = (lat: number, lng: number) => {
       });
     },
   );
+};
+
+export const getLatLngFromMapsURL = async (url: string) => {
+  try {
+    if (!locationSchema.shape.url.parse(url)) {
+      throw new Error("Invalid url");
+    }
+    const res = await axios.get(url, { maxRedirects: 0 });
+    const location = res.headers.location;
+    if (!location) {
+      throw new Error("No redirect location found");
+    }
+    const lat = Number(location?.match(/\!3d(-?\d+\.?\d*)/)?.[1]);
+    const lng = Number(location?.match(/\!4d(-?\d+\.?\d*)/)?.[1]);
+    return { lat, lng };
+  } catch (err) {
+    return null;
+  }
 };
