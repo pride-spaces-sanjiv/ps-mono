2864dbb54306672cdceb7fc145d292f1639c51d5
pride-spaces-sanjiv <sanjiv.samal@pridespaces.com>
2026-07-31 21:07:49 +0530

    changes : public operator APIs added

--- src/controllers/admin/operator.ts
+++ src/controllers/admin/operator.ts
@@ -1,28 +1,11 @@
 import { ResponseHandler } from "@/middlewares/request.js";
-import { Operator, operatorNonPassFields } from "@/database/models/operator.js";
 import * as generalControllers from "@/controllers/general/operator.js";
 import { handleMongooseError } from "@/utils/mongoose/error.js";
-import {
-  cleanPaginatedData,
-  paginatedResults,
-} from "@/utils/mongoose/pagination.js";
-import {
-  getFieldsandProjectors,
-  getSearchFilters,
-} from "@/utils/mongoose/filters.js";
-import { convertDataToJSON } from "@/utils/mongoose/conversion.js";
-import { cleanObject, deleteObjectFields } from "@/utils/object/clean.js";
-import { getSpaceCountsOfOperator } from "@/utils/mongoose/relations/space-operator.js";
-import { compareCryptos, decodeCrypto, encodeCrypto } from "@/utils/crypto.js";
-import { AdminLevel, adminLevels } from "@/utils/data/admin.js";
+import { compareCryptos } from "@/utils/crypto.js";
 // types
 import type { ManagedRequest, ManagedResponse } from "@/types/request.js";
 import { OperatorSchema } from "@/database/schemas/operator.js";
-import { ModelToDocument } from "@/types/mongoose/document.js";
 import { dumpStatuses } from "@/utils/data/dump.js";
-import { dumpAdminAction } from "@/utils/data/dumpAction.js";
-import { Types } from "mongoose";
-import { pipelineDBs } from "@/utils/services/pipeline/db.js";
 
 export const getOperators = async (
   req: ManagedRequest<any, { [k: string]: any }>,
---
+++ src/controllers/public/operator.ts
@@ -0,0 +1,32 @@
+import { ResponseHandler } from "@/middlewares/request.js";
+import * as generalControllers from "@/controllers/general/operator.js";
+// types
+import type { ManagedRequest, ManagedResponse } from "@/types/request.js";
+
+export const getOperators = async (
+  req: ManagedRequest<any, { [k: string]: any }>,
+  res: ManagedResponse,
+) => {
+  try {
+    await generalControllers.getOperators(req, res);
+  } catch (err) {
+    ResponseHandler.handleError(res, {
+      errorType: "get-operators-error-failure",
+      message: "Failed to get operators list",
+    });
+  }
+};
+
+export const getOperator = async (
+  req: ManagedRequest<any, { [k: string]: any }>,
+  res: ManagedResponse,
+) => {
+  try {
+    await generalControllers.getOperator(req, res);
+  } catch (err) {
+    ResponseHandler.handleError(res, {
+      errorType: "get-operator-error-failure",
+      message: "Failed to get operator details",
+    });
+  }
+};
