5748d4d563c2da91f51ee57f39f691be77865d23
Sanjiv39 <sanjiv.samal39@gmail.com>
2026-08-05 21:34:30 +0530

    changes : added operators bulk upload section

--- src/pages/migrations/index.tsx
+++ src/pages/migrations/index.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import MigrationsTabledResults from "@/containers/migrations-table";
-import SpaceMigrationsUploadSection from "@/containers/space/section/migration-upload";
+import MigrationsUploadSection from "@/containers/space/section/migration-upload";
 import { mediaTypes } from "@/utils/data/media";
 import { toast } from "sonner";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
@@ -13,12 +13,25 @@ import FormSectionTitle from "@/components/form/section/title";
 import { queryKeys } from "@/utils/query-keys";
 import { queryClient } from "@/App";
 
+// Sample links
+const centresSampleLink = new URL(
+  import.meta.env.VITE_RUSTFS_BASE.replace(/\/$/, "") +
+    "/pridespaces/samples/sample-centres-migration.xlsx",
+);
+const operatorsSampleLink = new URL(
+  import.meta.env.VITE_RUSTFS_BASE.replace(/\/$/, "") +
+    "/pridespaces/samples/sample-operators-migration.xlsx",
+);
+
 const MigrationsPage = () => {
   const { mutateAsync: uploadMutater, isPending: uploadPending } = useMutation({
     mutationFn: uploadMigrationFile,
   });
 
-  const handleFileUpload = async (file: UploadedFile) => {
+  const handleFileUpload = async (
+    file: UploadedFile,
+    migrationType: "spaces" | "operators" = "operators",
+  ) => {
     try {
       const formData = new FormData();
       formData.append("file", file.file);
@@ -26,7 +39,10 @@ const MigrationsPage = () => {
       formData.append("id", file.id);
       formData.append("contentType", file.file.type);
       formData.append("fileType", "migrationfile");
-      const res = await uploadMutater({ body: formData });
+      const res = await uploadMutater({
+        query: { for: migrationType },
+        body: formData,
+      });
       if (res.status === 201 && res?.data?.data?.files) {
         queryClient.invalidateQueries({ queryKey: [queryKeys.MIGRATIONS] });
         toast.success(
@@ -49,11 +65,46 @@ const MigrationsPage = () => {
       </div>
       <div className="py-4"></div>
       {/* <h1 className="text-lg font-semibold">Upload Migration Data </h1> */}
-      <SpaceMigrationsUploadSection
-        titleProps={{ children: "Upload Centres file" }}
+
+      {/* Operators */}
+      <MigrationsUploadSection
+        labels={{
+          title: "Operators Bulk File Upload",
+          description: "Drag & drop or click to upload operators bulk listings",
+          dndTitle: "Drop your file here",
+          dndBrowse: "browse file",
+        }}
+        sampleUrl={operatorsSampleLink}
+        titleProps={{ children: "Upload Operators CSV" }}
         processUpload={async (ref, file, setter) => {
           try {
-            const done = await handleFileUpload(file);
+            const done = await handleFileUpload(file, "operators");
+            if (!done) {
+              throw new Error("Incomplete");
+            }
+            ref.current?.click?.();
+            return {
+              status: "completed",
+            };
+          } catch (err) {
+            return { status: "error" };
+          }
+        }}
+      />
+
+      {/* Centres */}
+      <MigrationsUploadSection
+        labels={{
+          title: "Centres Bulk File Upload",
+          description: "Drag & drop or click to upload centres bulk listings",
+          dndTitle: "Drop your file here",
+          dndBrowse: "browse file",
+        }}
+        sampleUrl={centresSampleLink}
+        titleProps={{ children: "Upload Centres CSV" }}
+        processUpload={async (ref, file, setter) => {
+          try {
+            const done = await handleFileUpload(file, "spaces");
             if (!done) {
               throw new Error("Incomplete");
             }
